e29d6d66faa5b790f9c10173c8fb9935105d429d
Rafa Paradela <rafaparadela@gmail.com>
2014-06-11 04:22:20 +0200

    asdf

--- apidemo/urls.py
+++ apidemo/urls.py
@@ -11,9 +11,9 @@ urlpatterns = patterns('',
     url(r'^ironhackers/orphans$', 'api.endpoints.ironhackers.orphans'),
     url(r'^ironhackers/link_to_team$', 'api.endpoints.ironhackers.link_to_team'),
     
-    url(r'^teams/add$', 'api.endpoints.ironhackers.add'),
-    url(r'^teams/delete$', 'api.endpoints.ironhackers.delete'),
-    url(r'^teams/list$', 'api.endpoints.ironhackers.list'),
+    url(r'^teams/add$', 'api.endpoints.teams.add'),
+    url(r'^teams/delete$', 'api.endpoints.teams.delete'),
+    url(r'^teams/list$', 'api.endpoints.teams.list'),
 
 
     url(r'^admin/', include(admin.site.urls)),
